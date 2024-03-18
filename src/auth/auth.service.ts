52c9e0065007ce31c79483eda66b49b740386f6a
mostefa <mostefa.aliarous@gmail.com>
2024-03-18 13:10:56 +0100

    create auth module with service and controller

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { sign } from 'jsonwebtoken';
+import { AuthenticateDto } from './dto/authenticate.dto';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly userService: UserService) {} // Inject UserService
+
+  async authenticate(authenticateDto: AuthenticateDto){
+    const user = await this.userService.getUserByEmail(authenticateDto.email);
+    console.log(user)
+    if (!user) throw new NotFoundException('Invalid credentials');
+
+    const token = sign({ ...user }, 'secrete');
+
+    return { token, user };
+  }
+}
