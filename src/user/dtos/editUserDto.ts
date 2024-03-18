3cf56a601c5c3a0b327322215f89a501dec461ef
mostefa <mostefa.aliarous@gmail.com>
2024-03-18 06:28:21 +0100

    EditUserDto

---
+++ src/user/dtos/editUserDto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsEmail, IsNotEmpty} from 'class-validator';
+export class EditUserDto {
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+}
